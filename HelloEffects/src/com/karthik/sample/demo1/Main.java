906a87f82b4fcd9476f4b234543a8a786ac022b2
Karthik.k <Karthik.k@Adeptpros-D-23.adeptpros.com>
2014-11-04 11:42:10 +0530

    commit 1155

--- HelloEffects/src/com/karthik/sample/demo1/Main.java
+++ HelloEffects/src/com/karthik/sample/demo1/Main.java
@@ -4,6 +4,7 @@ import com.example.android.mediafx.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Main extends Activity {
 @Override
@@ -11,6 +12,6 @@ protected void onCreate(Bundle savedInstanceState) {
 	// TODO Auto-generated method stub
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
-	
+	GridView gridView= (GridView) findViewById(R.id.gridOne);
 }
 }
