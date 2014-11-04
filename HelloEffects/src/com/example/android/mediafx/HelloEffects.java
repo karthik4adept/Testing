090fa88b10f4cd9c6dce57431234524427ee04d2
karthik4adept <karthik@grr.la>
2014-11-04 10:23:05 +0530

    Second Commit

--- HelloEffects/src/com/example/android/mediafx/HelloEffects.java
+++ HelloEffects/src/com/example/android/mediafx/HelloEffects.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Sample Work Done by Karthik
  */
 
 package com.example.android.mediafx;
