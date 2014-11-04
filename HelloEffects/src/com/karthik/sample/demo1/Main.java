854e028c5be6c9e528005acb03f849d221a56458
Karthik.k <Karthik.k@Adeptpros-D-23.adeptpros.com>
2014-11-04 10:41:14 +0530

    adding new Java file

---
+++ HelloEffects/src/com/karthik/sample/demo1/Main.java
@@ -0,0 +1,15 @@
+package com.karthik.sample.demo1;
+
+import com.example.android.mediafx.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+}
+}
